1b47000da52f8c6dcb771923c935dd87f22abee0
Ramon Cardoso de Oliveira <Ramon.Oliveira@mercanet.com.br>
2024-11-18 10:54:12 -0300

    ajuste de layout

--- pages/index.vue
+++ pages/index.vue
@@ -8,20 +8,18 @@
                     <FormsComboBox class="tw-w-[150px]" v-model="store.tipoCalculo" :options="options"
                         placeholder="Tipo de Ação" />
                     <span>Horas: {{ decimalToFormatHoursMinutos(store.totalResultado) }} </span>
+                    <FormsInputText v-model="store.tag" placeholder="Tag" class="tw-w-28" />
+                    <Btn text="Adicionar" icon="radix-icons:paper-plane" @click="store.addHistorico()" />
                 </div>
-                <div class="tw-flex tw-gap-3 tw-items-center">
-                    <FormsInputText v-model="store.tag" placeholder="Tag" />
-                    <FormsInputText v-model="store.formato" placeholder="formatação" />
-                    <span>Formato: {{ formatCustomHours(store.totalResultado, store.formato) }}</span>
-                </div>
-            </div>
-            <div class="tw-flex tw-items-center tw-border-l-2 tw-border-neutral-200 dark:tw-border-white/10 tw-px-3">
-                <Btn text="Adicionar" icon="radix-icons:paper-plane" @click="store.addHistorico()" />
             </div>
         </div>
         <div class="tw-w-[58rem] tw-mt-3">
             <div role="toolbar" class="tw-py-4">
                 <Btn variant="secondary" text="Limpar" icon="radix-icons:eraser" @click="store.limparHistorico()" />
+                <span class="tw-mx-2">
+                    <span class="tw-text-xl tw-font-bold">Formatação: </span>
+                    <FormsInputText class="tw-w-28" v-model="store.formato" placeholder="formatação" />
+                </span>
                 <span class="tw-text-xl tw-font-bold">
                     Total Horas: {{ decimalToFormatHoursMinutos(store.totalHistorico) }}
                 </span>
