2c80172f2a018f2e39a9bc6c34a0009fe2f24f95
Ramon Cardoso de Oliveira <Ramon.Oliveira@mercanet.com.br>
2025-04-22 13:25:50 -0300

    melhoria de layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
     <nav class="tw-fixed tw-left-0 tw-right-0 tw-top-0 tw-bg-white tw-border-gray-200 dark:tw-bg-gray-900 tw-shadow-md">
-        <div
-            class="tw-flex tw-flex-wrap tw-justify-between tw-items-center tw-mx-auto tw-p-4 xl:tw-px-44 lg:tw-px-22 md:tw-px-4 sm:tw-px-2">
+        <div class="tw-flex tw-flex-wrap tw-justify-between tw-items-center tw-mx-auto tw-p-4 xl:tw-px-44 lg:tw-px-22">
             <div class="tw-flex tw-items-center ">
                 <NuxtLink to="/" class="tw-flex tw-items-center tw-space-x-3 rtl:tw-space-x-reverse">
                     <img src="/assets/img/logo.png" class="tw-h-8" alt="Flowbite Logo" />
@@ -11,7 +10,7 @@
 
                 <div class="tw-ml-8 tw-flex tw-align-bottom" id="navbar-default">
                     <ul
-                        class="dark:tw-text-white tw-font-medium tw-flex tw-flex-col tw-p-4 md:tw-p-0 tw-mt-4 tw-rounded-lg md:tw-flex-row md:tw-space-x-8 tw-rtl:tw-space-x-reverse md:tw-mt-0 tw-dark:tw-bg-gray-800 md:tw-dark:tw-bg-gray-900">
+                        class="dark:tw-text-white tw-font-medium tw-flex tw-flex-col md:tw-p-0 tw-mt-4 tw-rounded-lg tw-rtl:tw-space-x-reverse md:tw-mt-0 tw-dark:tw-bg-gray-800 md:tw-dark:tw-bg-gray-900">
 
                         <li>
                             <NuxtLink to="/ultis" class="tw-flex tw-items-center">
@@ -24,7 +23,7 @@
             <div class="tw-flex tw-items-center tw-space-x-6 rtl:tw-space-x-reverse">
                 <div class="tw-ml-8 tw-flex tw-align-bottom" id="navbar-default">
                     <ul
-                        class="dark:tw-text-white tw-font-medium tw-flex tw-flex-col tw-p-4 md:tw-p-0 tw-mt-4 md:tw-flex-row md:tw-space-x-8 tw-rtl:tw-space-x-reverse md:tw-mt-0 tw-dark:tw-bg-gray-800 md:tw-dark:tw-bg-gray-900">
+                        class="dark:tw-text-white tw-font-medium tw-flex tw-flex-col md:tw-p-0 tw-mt-4 tw-rtl:tw-space-x-reverse md:tw-mt-0 tw-dark:tw-bg-gray-800 md:tw-dark:tw-bg-gray-900">
                         <li>
                             <NuxtLink to="/settings" class="tw-flex tw-items-center">
                                 Configurações
