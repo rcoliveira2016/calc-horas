f690f1d127f4c6bf46948d36cf8488a07cf96c85
Ramon Cardoso de Oliveira <Ramon.Oliveira@mercanet.com.br>
2025-06-14 12:58:19 -0300

    melhorias no env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'reka-ui/nuxt', '@pinia/nuxt'],
+  modules: ["@nuxtjs/tailwindcss", "reka-ui/nuxt", "@pinia/nuxt"],
   app: {
     head: {
-      title: 'Calc Horas',
+      title: "Calc Horas",
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Calc Horas' },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "Calc Horas" },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
   runtimeConfig: {
-    githubClientSecret: '',
+    githubClientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
     public: {
-      githubClientId: '',
+      githubClientId: process.env.NUXT_PUBLIC_GITHUB_CLIENT_ID,
       baseURL: `http://localhost:${process.env.PORT || 3000}`,
     },
   },
-})
+});
